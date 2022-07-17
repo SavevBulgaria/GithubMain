71f33839ee73d53055232990138957b4e2be101e
SavevBulgaria <109469306+SavevBulgaria@users.noreply.github.com>
2022-07-17 17:22:49 +0300

    Add files via upload
    Homework 7

---
+++ tema7/zad1.java
@@ -0,0 +1,32 @@
+package tema7;
+
+import java.util.Scanner;
+
+public class zad1 {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        double avrg = 0;
+        int sum = 0;
+        String text = scanner.nextLine();
+
+        String[] splittext = text.split(" ");
+        int[] newArray = new int[splittext.length];
+        int i;
+        for (i = 0; i < newArray.length; i++) {
+            newArray[i] = Integer.parseInt(splittext[i]);
+            sum += newArray[i];
+            min = Math.min(min, newArray[i]);
+            max = Math.max(max, newArray[i]);
+        }
+
+        avrg = (double) sum / newArray.length;
+        System.out.println("min" + min);
+        System.out.println("max" + max);
+        System.out.println("avrg" + avrg);
+        System.out.println("sum" + sum);
+    }
+}
