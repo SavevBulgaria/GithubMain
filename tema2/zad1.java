4c2eac6846ce321278e24285b7c2f5ba6da6746d
SavevBulgaria <109469306+SavevBulgaria@users.noreply.github.com>
2022-07-17 17:05:21 +0300

    Add files via upload

---
+++ tema2/zad1.java
@@ -0,0 +1,38 @@
+package tema2;
+
+import java.util.Scanner;
+
+public class zad1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = Integer.parseInt(input.nextLine());
+        int povtorenia = n * 2;
+        int L = 0, R = 0;
+
+        for (int i = 0; i < povtorenia; i++) {
+            if (i == 0) {
+                System.out.println("Enter Left sum");
+            } else if (i == n) {
+                System.out.println("Enter Right sum");
+            }
+            if (i < n) {
+                int num1 = Integer.parseInt(input.nextLine());
+            } else if (i >= n) {
+                int num2 = Integer.parseInt(input.nextLine());
+
+                int dif = 0;
+                if (R > L) {
+                    dif = R - L;
+                    System.out.println("Difference=" + dif);
+                } else if (L > R) {
+                    dif = L - R;
+                    System.out.println("Difference=" + dif);
+
+                }
+
+            }
+        }
+
+    }
+}
---
+++ tema2/zad3.java
@@ -0,0 +1,26 @@
+package tema2;
+
+
+import java.util.Scanner;
+
+public class zad3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("n = ");
+        String s = scanner.nextLine();
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') {
+                sum += 1;
+            } else if (s.charAt(i) == 'e') {
+                sum += 2;
+            } else if (s.charAt(i) == 'o') {
+                sum += 3;
+            } else if (s.charAt(i) == 'u') {
+                sum += 5;
+
+            }
+        }
+        System.out.println("Vowels sum = " + sum);
+    }
+}
