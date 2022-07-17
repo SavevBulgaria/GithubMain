3ab8a6d83b674db2df2140449da1ebe1be4d0f11
Denis Savev <109469306+SavevBulgaria@users.noreply.github.com>
2022-07-17 19:10:43 +0300

    Add files via upload

---
+++ ClassWork1/ex1.java
@@ -0,0 +1,17 @@
+package tema1;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+
+        for (short i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            } else {
+                System.out.println(i);
+            }
+
+
+        }
+    }
+}
---
+++ ClassWork1/ex4.java
@@ -0,0 +1,19 @@
+package tema1;
+
+import java.util.Scanner;
+
+public class ex4 {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        int n= Integer.parseInt(input.nextLine());
+        int largestNum = Integer.MIN_VALUE;
+
+        for(int i=0;i<n;i++){
+            int num =Integer.parseInt(input.nextLine());
+            if(num>largestNum)largestNum=num;
+
+        }
+        System.out.println("Largest Num"+largestNum);
+
+    }
+}
