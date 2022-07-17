8d31d2f4d5de0edef3e4d9d52163c0a131535d49
Denis Savev <109469306+SavevBulgaria@users.noreply.github.com>
2022-07-17 19:10:24 +0300

    Add files via upload

---
+++ FirstSteps/ex2.java
@@ -0,0 +1,15 @@
+package FirstSteps;
+
+public class ex2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+
+
+        }
+
+
+    }
+
+}
