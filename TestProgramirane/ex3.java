6810c27a5183ebec175bd90ce98bf1e0a7de9aad
Denis Savev <109469306+SavevBulgaria@users.noreply.github.com>
2022-07-17 19:15:04 +0300

    Add files via upload

---
+++ TestProgramirane/ex3.java
@@ -0,0 +1,19 @@
+package TestProgramirane;
+
+
+public class ex3 {
+    public static void main(String[] args) {
+
+        int i;
+        for (i = 50; i <= 198; i ++) {
+            if(i % 2 !=0) {
+                System.out.print(i + ",");
+
+            }
+        }
+System.out.print(i);
+
+    }
+
+
+}
