b3c08611d1f175ee02a3dd0ae86ea03d800d666e
SavevBulgaria <109469306+SavevBulgaria@users.noreply.github.com>
2022-07-17 17:29:46 +0300

    Add files via upload
    Fun little unfinished project

---
+++ FUNTIME.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class FUNTIME {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        HashMap<Character,Character> Alpha=new HashMap<Character, Character>();
+       // A, B, C, D, E, F, G, H, I, J!!!!, K, L, M, N, O, P, !!!!Q, R, S, T, U, V, W, X, Y and Z.
+        Alpha.put('A','Z');
+        Alpha.put('B','Y');
+        Alpha.put('C','X');
+        Alpha.put('D','W');
+        Alpha.put('E','V');
+        Alpha.put('F','U');
+        Alpha.put('G','T');
+        Alpha.put('H','S');
+        Alpha.put('I','R');
+        Alpha.put('J','Q');
+        Alpha.put('K','P');
+        Alpha.put('L','O');
+        Alpha.put('M','N');
+
+      System.out.println();
+
+
+
+
+
+        }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
