8374a0ce5b80071ecb2d5f3fce2ddc8305d319ae
SavevBulgaria <109469306+SavevBulgaria@users.noreply.github.com>
2022-07-17 17:28:52 +0300

    Add files via upload
    First Test of the year

---
+++ TestProgramirane/zad1.java
@@ -0,0 +1,14 @@
+package TestProgramirane;
+
+import java.util.Scanner;
+
+public class zad1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+    }
+}
---
+++ TestProgramirane/zad5.java
@@ -0,0 +1,29 @@
+package TestProgramirane;
+
+import java.util.Scanner;
+
+public class zad5 {
+    public static void main(String[] args) {
+        //*****
+//         ***
+//          *
+        Scanner scanner = new Scanner(System.in);
+        int stars = scanner.nextInt();
+        int spaces = 1;
+        int n = stars;
+        for (int i = 0; i < n; i++) {
+            for (int d = 0; d < stars; d++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(" ");
+            }
+            stars -= 2;
+            spaces++;
+        }
+
+
+    }
+}
